0f12bab18b48a6099ca1e2ce7d5d4756644d84c4
Thomas <thomas.reverat@gmail.com>
2022-12-14 11:49:00 +0100

    first commit

---
+++ autoClic/autoClic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  autoClic
+//
+//  Created by Thomas Reversat on 14/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
